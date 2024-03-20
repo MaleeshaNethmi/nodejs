d25617ab2ea84842703503d2d5475806f30ce15b
MaleeshaNethmi <Mnethmi27@gmail.com>
2024-03-20 17:41:42 +0530

    set value

--- src/models/customer.js
+++ src/models/customer.js
@@ -2,7 +2,7 @@ const mongoose = require ( 'mongoose' );
 
 const customerSchema = new mongoose.Schema({
     name: {
-        type: String,
+        type: String, 
         required: true
     },
     industry: String,
