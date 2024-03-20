afc63d0738b940f9953e189bfc28d72222c9d714
MaleeshaNethmi <Mnethmi27@gmail.com>
2024-03-20 17:44:02 +0530

    fix syntax error

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ mongoose.set('strictQuery', false );
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true}));
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
